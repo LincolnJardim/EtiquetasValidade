95d571f68b1ba9ca85979447c04022d58ede96aa
Lincoln Jardim <linconl11@hotmail.com>
2026-05-21 14:21:02 -0300

    Primeira página finalizada.

--- backend/Etiquetas.Api/obj/Debug/net10.0/Etiquetas.Api.AssemblyInfo.cs
+++ backend/Etiquetas.Api/obj/Debug/net10.0/Etiquetas.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etiquetas.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e397084f7c48537fd22fe98d52f7b2c5fb384547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aeea10aadcd46498cc8fd3fa26de28458f1bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etiquetas.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etiquetas.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Etiquetas.Domain/obj/Debug/net10.0/Etiquetas.Domain.AssemblyInfo.cs
+++ backend/Etiquetas.Domain/obj/Debug/net10.0/Etiquetas.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etiquetas.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e397084f7c48537fd22fe98d52f7b2c5fb384547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aeea10aadcd46498cc8fd3fa26de28458f1bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etiquetas.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etiquetas.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
