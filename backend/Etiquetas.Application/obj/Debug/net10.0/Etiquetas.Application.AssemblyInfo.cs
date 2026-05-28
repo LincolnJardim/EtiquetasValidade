71c873f4dc86263934fae7eb2af6fd7fb62dcda7
Lincoln Jardim <linconl11@hotmail.com>
2026-05-28 14:14:05 -0300

    "Restruturação do html e css da página de cadastro de produtos. Criação da tela "Lista de produtos".

--- backend/Etiquetas.Application/obj/Debug/net10.0/Etiquetas.Application.AssemblyInfo.cs
+++ backend/Etiquetas.Application/obj/Debug/net10.0/Etiquetas.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d571f68b1ba9ca85979447c04022d58ede96aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1cd067d9d8b0da0b0e1d16894aec12438afe5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
