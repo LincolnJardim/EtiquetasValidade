857bbf6d5fa8ad705ed039101d6e85830ea9ebf4
Lincoln Jardim <linconl11@hotmail.com>
2026-07-02 12:07:11 -0300

    Adicionado a tela cadastro de produção e implementação do método POST para produção. Funcionando corretamente.

--- backend/Etiquetas.Application/obj/Debug/net10.0/Etiquetas.Application.AssemblyInfo.cs
+++ backend/Etiquetas.Application/obj/Debug/net10.0/Etiquetas.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff5d0f2f02b1eaaf526ac124686ddd5901f066d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27720143a9c92e7d7d72966173899455048db66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etiquetas.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/etiquetas-html/javascript/criarProducao.js
@@ -0,0 +1,95 @@
+// Manipulação do DOM para esperar a página HTML carregar por completo antes de chamar a função cadastrarProduto()
+document.addEventListener('DOMContentLoaded', function () {
+    carregarProduto()
+    preencherDataAtual()
+    cadastrarProducao()
+})
+
+async function carregarProduto() {
+    const dropdownProdutos = document.getElementById('idrop-produto')
+
+    
+    try {
+
+        const resposta = await fetch('https://localhost:7288/Produto/listarProdutosCadastrados',
+            {
+                method: 'GET'
+            }
+        )
+
+        if (resposta.ok) {
+            let listaProdutos = await resposta.json()
+
+            
+            
+            for (let produto of listaProdutos) {
+
+                let item = document.createElement('option')
+                item.text = `${produto.nome} (${produto.diasValidade} dias)`
+                item.value = produto.id
+
+
+                dropdownProdutos.appendChild(item)
+
+            }
+        }
+
+    } catch (erro) {
+        console.error('Erro de rede: A API pode estar desligada ou fora do ar.', erro)
+    }
+}
+
+function preencherDataAtual() {
+    let inputData = document.getElementById('idatafabricacao')
+    let data = new Date()
+
+    let ano = data.getFullYear()
+    let mes = String(data.getMonth() + 1).padStart(2, '0')
+    let dia = String(data.getDate()).padStart(2, '0')
+
+    let dataFormatada = `${ano}-${mes}-${dia}`
+
+    inputData.value = dataFormatada
+
+}
+
+function cadastrarProducao() {
+
+    const formulario = document.getElementById('icadastro')
+
+    formulario.addEventListener('submit', async function(evento) {
+        evento.preventDefault()
+
+        let produto = document.getElementById('idrop-produto').value
+        let dataFabricacao = document.getElementById('idatafabricacao').value
+        let quantidadeEtiquetas = document.getElementById('ietiquetas').value
+
+        let producaoJson = {
+            produtoId: produto,
+            dataFabricacao: dataFabricacao,
+            quantidadeEtiquetas: Number(quantidadeEtiquetas)
+        }
+
+        try {
+            const resposta = await fetch('https://localhost:7288/Producao', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(producaoJson)
+            })
+
+            if (resposta.ok) {
+                window.alert(`A produção ${producaoJson.produtoId} foi cadastrada com sucesso.`)
+            } else {
+                window.alert('O servidor C# recebeu, mas retornou um erro.')
+            }
+        } catch (erro) {
+            console.error('Erro de rede: A API pode estar desligada ou fora do ar.', erro)
+        }
+
+        formulario.reset()
+
+        preencherDataAtual()
+    }) 
+}
